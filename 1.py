562712cb75b89b65065dc3475194ab1442012158
DavidCMathew <davidsemailid@gmail.com>
2018-11-26 16:29:12 +0530

    Initial Commit Program 1 added

---
+++ 1.py
@@ -0,0 +1,22 @@
+traindata=[]
+with open('data/enjoysport.csv') as f:
+    for line in f:
+        traindata.append(line.split(','))
+
+rows = len(traindata)
+cols = len(traindata[0])
+
+h = ['0'] * cols
+
+for i in range(1, rows):
+    t = traindata[i]
+    if t[cols-1] == '0':
+        continue
+    for y in range(0, cols-1):
+        if h[y] == t[y]:
+            continue
+        h[y] = t[y] if h[y] == '0' else '?'
+    print(h)
+
+print('The maximally specific set is ')
+print(str(h[:-1]).replace('[', '<').replace(']', '>'))
---
+++ README.md
@@ -0,0 +1,3 @@
+# Machine Learning Lab Programs
+### Overview
+The repo contains the 10 lab programs and the required data files.
