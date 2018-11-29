af301f93efb2019c1a782add663127d3c42a90c5
DavidCMathew <davidsemailid@gmail.com>
2018-11-29 17:47:56 +0530

    minor changes

--- 9.py
+++ 9.py
@@ -4,10 +4,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 iris_dataset = load_iris()
+targets = iris_dataset.target_names
 
 print("Class : number")
-for i in range(len(iris_dataset.target_names)):
-    print(iris_dataset.target_names[i], ':', i)
+for i in range(len(targets)):
+    print(targets[i], ':', i)
 
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset["data"], iris_dataset["target"])
 kn = KNeighborsClassifier(1)
@@ -16,6 +17,6 @@ kn.fit(X_train, y_train)
 for i in range(len(X_test)):
     x_new = np.array([X_test[i]])
     prediction = kn.predict(x_new)
-    print("Actual:{0} {1},Predicted:{2}{3}".format(y_test[i], iris_dataset["target_names"][y_test[i]], prediction, iris_dataset["target_names"][prediction]))
+    print("Actual:[{0}] [{1}],Predicted:{2} {3}".format(y_test[i], targets[y_test[i]], prediction, targets[prediction]))
 
 print("\nAccuracy: ", kn.score(X_test, y_test))
